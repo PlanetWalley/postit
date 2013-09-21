d6cfafb93f191746feef95b01c6e2a3020276034
nilyibo <yibo@Yibos-Mac.local>
2013-09-21 01:13:00 -0500

    Local Android build

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/yibo/Documents/PhoneGap/postit/platforms/android/bin/AndroidManifest.xml : \
-/Users/yibo/Documents/PhoneGap/postit/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/PostIt-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/yibo/Documents/PhoneGap/postit/platforms/android/bin/PostIt-debug-unaligned.apk : \
-/Users/yibo/Documents/PhoneGap/postit/platforms/android/bin/PostIt.ap_ \
-/Users/yibo/Documents/PhoneGap/postit/platforms/android/bin/classes.dex \
-/Users/yibo/Documents/PhoneGap/postit/platforms/android/libs/cordova-3.0.0.jar \
