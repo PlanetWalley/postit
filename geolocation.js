fec73af1ad0000fa76e8e83f3c309865d7d8b63e
shd101wyy <shd101wyy@sina.com>
2013-09-22 03:21:29 -0400

    very good

--- geolocation.js
+++ geolocation.js
@@ -1,31 +1,70 @@
 
 // if can show position
+var longitude = 0; // current longitude
+var latitude = 0;  // current latitude
+var Google_Map = null;
 // call this function
 var showPosition = function(position){
     // get longitude
-    var longitude = position.coords.longitude
+    longitude = position.coords.longitude
     // get latitude
-    var latitude = position.coords.latitude
+    latitude = position.coords.latitude
     document.getElementById("latInfo").innerHTML = "Latitude: " + latitude;
     document.getElementById("lonInfo").innerHTML = "Longitude: " + longitude;
     //console.log("Longitude: "+longitude)
     //console.log("Latitude: "+latitude)
-}
+    
+    // google map
+    var latlon=new google.maps.LatLng(latitude, longitude)
+    var mapholder=document.getElementById('mapholder')
+      //mapholder.style.height='100px';
+      //mapholder.style.width='100px';
+      //mapholder.style.border-radius='250px';
+
+    var myOptions={
+          draggable:false,
+          disableDoubleClickZoom:true,
+          panControl:false,
+          zoomControl:false,
+          mapTypeControl:false,
+          scaleControl:false,
+          streetViewControl:false,
+          overviewMapControl:false,
+          rotateControl:false,
+          center:latlon,
+          zoom:18,
+          mapTypeId:google.maps.MapTypeId.ROADMAP,
+          mapTypeControl:false,
+          navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+      };
+
+      if (Google_Map == null){
+        Google_Map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+      }
+      else{
+        Google_Map.setCenter(latlon)
+      }
+      //Google_Map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+      var marker=new google.maps.Marker({position:latlon,map:Google_Map,title:"You are here!"});
+      google.maps.event.addListener(marker, 'click', function() {
+            alert("U clicked me")
+         });
+      }
 
 // print error message
 function showError(error){
     switch(error.code) 
     {
-    case error.PERMISSION_DENIED:
+        case error.PERMISSION_DENIED:
         alert("User denied the request for Geolocation.")
         break;
-    case error.POSITION_UNAVAILABLE:
+        case error.POSITION_UNAVAILABLE:
         alert("Location information is unavailable.")
         break;
-    case error.TIMEOUT:
+        case error.TIMEOUT:
         alert("The request to get user location timed out.")
         break;
-    case error.UNKNOWN_ERROR:
+        case error.UNKNOWN_ERROR:
         alert("An unknown error occurred.")
         break;
     }
@@ -34,10 +73,9 @@ function showError(error){
 // check geolocation support 
 if (navigator.geolocation){
     navigator.geolocation.getCurrentPosition(showPosition, showError,
-                                             
                                              {timeout:1000 ,// update every 1 sec 
                                               enableHighAccuracy: true} // enable high accuracy
-                                            )
+                                              )
 }
 else{
     alert("Sorry, Ur browser does not support geolocation")
---
+++ offline_post.js
@@ -0,0 +1,51 @@
+var user_name = "planetwalley" // user name
+var information = {} // information that will be stored in local, categorized by user name
+
+
+var createPostInformation = function(user_longitude, user_latitude, user_message, post_date, user_name){
+	/*
+		sample that will be saved to information
+		if user post
+	*/
+	var post_information = {
+		latitude:user_latitude, 
+		longitude:user_longitude,
+		message:user_message,
+		time:post_date
+	}
+
+	if (information[user_name]===undefined)
+		information[user_name] = {}
+
+	information[user_name][post_date] = post_information
+
+	// save information local storage
+	if(window.localStorage){
+		window.localStorage["postit"] = information
+		alert("Successfully PostIt!")
+
+		// get icon from google
+		var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+		// init latlon
+		var latlon=new google.maps.LatLng(latitude, longitude)
+		// set marker
+		var marker=new google.maps.Marker({position:latlon,
+										   map:Google_Map,
+										   icon:iconBase + 'schools_maps.png', // set icon
+										   title:user_name+":"+post_date}); 
+		google.maps.event.addListener(marker, 'click', function() {
+                                    alert(user_name + ": "+information[user_name][post_date]["message"])
+                                });
+
+	}
+	else{
+		alert("Cannot post cuz ur browser does not support local storage")
+	}
+}
+
+
+var PostIt = function(){
+	var message = prompt("Enter the message u want to post")
+	createPostInformation(longitude, latitude, message, new Date(), user_name)
+
+}
