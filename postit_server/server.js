df68dd4c45e641aac64a2c8647b676a1ec2fd032
shd101wyy <shd101wyy@sina.com>
2013-09-22 10:49:07 -0400

    fix one online version bug

--- postit_server/server.js
+++ postit_server/server.js
@@ -3,6 +3,7 @@ var app = require('http').createServer(handler)
   , fs = require('fs')
 
 app.listen(9001);
+var information = {}
 
 
 function handler (req, res) {
@@ -19,12 +20,23 @@ function handler (req, res) {
 
 io.sockets.on('connection', function (socket) {
   console.log("User Connected")
-  socket.emit('news', { hello: 'world' });
+
+  socket.emit('updateMapForFirstTimeConnectedUser', information);
+
   socket.on('my other event', function (data) {
     console.log(data);
   });
   socket.on('User_Sent_Message',function(data){
     console.log("Get Post Message")
+    var longitude = data[0]
+    var latitude = data[1]
+    var user_message = data[2]
+    var user_name = data[3]
+    var post_date = data[4]
+    if (information[user_name] === undefined)
+      information[user_name]={}
+    information[user_name][post_date] = [longitude,latitude,user_message,user_name,post_date]
+
     io.sockets.emit('Update_Map', data);
   })
 });
